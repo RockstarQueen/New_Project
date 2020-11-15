8da0b70634433639aa648b7f38a6b6029dea4733
RockstarQueen <p12172@naver.com>
2020-11-15 21:21:19 +0900

    new button

--- MrJackInLondon/resource.h
+++ MrJackInLondon/resource.h
@@ -15,6 +15,7 @@
 #define IDB_BITMAP_PLAY_ON              158
 #define IDB_BITMAP_RULE_DEF             159
 #define IDB_BITMAP_RULE_ON              160
+#define IDB_BITMAP1                     165
 #define IDC_BT_START                    1000
 #define IDC_BT_HTP                      1002
 #define IDC_BUTTON2                     1005
@@ -23,14 +24,15 @@
 #define IDC_HTPB_NEXT                   1007
 #define IDC_BUTTON1                     1008
 #define IDC_IGB_HELP                    1008
+#define IDC_BUTTON_SETTING              1009
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        162
+#define _APS_NEXT_RESOURCE_VALUE        166
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
