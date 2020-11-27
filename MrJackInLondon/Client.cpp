480341d428ef29c4f03ee27936c8b8a90647ea76
hyunwoo9120 <hyunwoo.k9120@gmail.com>
2020-11-27 10:36:08 +0900

    receive data

---
+++ MrJackInLondon/Client.cpp
@@ -0,0 +1,23 @@
+﻿// Client.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "MrJackInLondon.h"
+#include "Client.h"
+
+
+// CClient
+
+CClient::CClient()
+{
+	// 초기화를 해야 10093 에러가 발생하지 않는다
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+}
+
+CClient::~CClient()
+{
+}
+
+
+// CClient 멤버 함수
---
+++ MrJackInLondon/Client.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#define PORT 18363
+#define PACKET_SIZE 1024
+#define IP "---.---.---.---"
+
+// CClient 명령 대상
+
+class CClient : public CSocket
+{
+public:
+	CClient();
+	virtual ~CClient();
+};
+
+
